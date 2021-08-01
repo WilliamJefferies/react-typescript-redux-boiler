4edb0faaad9f00075b5955fa9e40789710305b54
William Jefferies <william@checkferret.com>
2021-08-01 12:45:55 +1000

    initial commit

---
+++ src/state/actions/index.ts
@@ -0,0 +1,17 @@
+import {ActionType} from '../action-types'
+
+interface DepositAction {
+    type: ActionType.DEPOSIT,
+    payload: number
+}
+
+interface WithdawAction {
+    type: ActionType.WITHDRAW
+    payload: number
+}
+
+interface BankruptAction {
+    type: ActionType.BANKRUPT
+}
+
+export type Action = DepositAction | WithdawAction | BankruptAction
